c9845c8387fe218160acad780740bd48c6a09f8a
Cheeseburger <wwj.woo@gmail.com>
2017-11-20 23:08:30 +0100

    Update getData.py
    Update getData function.

--- getData.py
+++ getData.py
@@ -1,13 +1,17 @@
 # https://api.steampowered.com/<interface>/<method>/v<version>/
-import http.client
+import urllib.request
 import json
 
 key = "your steam-web-api key"
 
-def getData(key, userID, interface, method, version = ""):
-    conn = http.client.HTTPSConnection('api.steampowered.com')
-    conn.request('GET', '/' + interface + '/' + method + '/v' + version + '/?format=json&key=' + key + '&steamids=' + userID)
-    resp = conn.getresponse()
+def getData(key, userID, interface, method, ids='ids', version="1"):
+    url = "https://api.steampowered.com/" + interface + '/' + method + '/v' + version + '/?format=json&key=' + key + '&steam' + ids + '=' + userID
+    resp = urllib.request.urlopen(url)
     data = json.loads(resp.read().decode('utf-8'))
-
     return data
+
+# get user data
+# getData(key, userID, "ISteamUser", "GetPlayerSummaries")
+
+# get ownedgames and total played time
+# getData(key, userID, "IPlayerService", "GetOwnedGames", "id")
